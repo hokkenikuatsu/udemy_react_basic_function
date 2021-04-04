8c51bfb491a5c4a6e46c30739aea1167831e45b8
hokkenikuatsu <68536660+hokkenikuatsu@users.noreply.github.com>
2021-04-04 23:35:11 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,207 @@
+/**
+ * const., letの変数宣言
+ */
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数上書き";
+// console.log(val1);
+
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// letは再宣言不可能
+// let val2 = "let変数を再度宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// const変数は上書き不可能
+// val3 = "const変数を上書き";
+
+// const val3 = "const変数を際宣言";
+
+// // constで定義したobjectはプロパティの変更が可能
+// const val4 = {
+//   name: "ほし",
+//   age: 28
+// };
+// console.log(val4);
+
+// val4.name = "ほしalpha";
+// console.log(val4);
+// val4.adress = "Tokyo";
+// console.log(val4);
+
+// // constで定義した配列はプロパティの変更が可能
+// const val5 = ["dog", "cat"];
+// console.log(val5);
+// val5[0] = "monkey";
+// console.log(val5);
+// val5.push("cow");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+// const name = "ほし";
+// const age = 28;
+// // 「私の名前はほしです。年連は28歳です。」
+
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
+
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+// 従来の関数
+// function func1(str) {
+//   return str;
+// }
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です。"));
+
+// // アロー関数
+// const func2 = (str) => str;
+// console.log(func2("func2です。"));
+
+// const func3 = (num1, num2) => num1 + num2;
+// console.log(func3(1, 1000));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "ほし",
+//   age: 28
+// };
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// const myProfile = ["ほし", 28];
+// console.log(`名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`);
+// const [name, age] = myProfile;
+// const message3 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message3);
+
+/**
+ * デフォルト値、引数など
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは! ${name}さん!`);
+// sayHello();
+// sayHello("ほし");
+
+/**
+ * スプレッド構文 ...
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1, num2);
+// console.log(arr3);
+
+// 配列のコピー, 結合
+// const arr4 = [10, 20];
+
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr4, arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8 = arr4;
+// console.log(arr8);
+// arr8[0] = 100;
+// console.log(arr4[0], arr8[0]);
+
+/**
+ * map, filterを用いた配列の処理
+ */
+// const nameArr = ["ほし", "しゅうと", "ぶた", "ぶひー"];
+// nameArr
+//   .filter((name) => name === "ほし")
+//   .map((name, index) => console.log(`${index + 1}番目は名前は${name}です。`));
+
+// const nameArr2 = nameArr.map((name, index) => {
+//   return `index ${index} sample name is ${name}`;
+// });
+// console.log(nameArr2);
+
+// const nameArr3 = nameArr.filter((name) => {
+//   return name === "ぶひー";
+// });
+// console.log(nameArr3);
+
+/**
+ * 参考演算子
+ */
+// ある条件 ? 条件がtrueの場合 : 条件がfalseの場合;
+// const val1 = 1 < 0 ? `trueです` : `falseです`;
+// console.log(val1);
+
+// const num = "1300";
+// // console.log(num.toLocaleString());
+
+// const formattedNum =
+//   typeof num === `number` ? num.toLocaleString() : `数字を入力せよ`;
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? `100を超えています!` : `許容範囲内です。`;
+// };
+// console.log(checkSum(10, 20));
+// console.log(checkSum(100, 20));
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = true;
+
+// if (flag1 || flag2) {
+//   console.log(`1か2はtrueです。`);
+// }
+
+// if (flag1 && flag2) {
+//   console.log(`1も2もtrueです。`);
+// }
+
+// || は左側がfalseなら右側を返す
+// const num = 100;
+// const fee = num || `金額未設定です。`;
+// console.log(fee);
+
+// &&は左側がtrueなら右側を返す
+const num2 = 100;
+const fee2 = num2 && `何か設定されました。`;
+console.log(fee2);
